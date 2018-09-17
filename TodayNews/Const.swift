b384b3fdd2f33c6351baa40c1b2164518834ca47
lck93 <1012439550@qq.com>
2018-09-17 15:41:40 +0800

    bug

--- TodayNews/Const.swift
+++ TodayNews/Const.swift
@@ -10,7 +10,7 @@ import UIKit
 
 let SCREENWIDTH = UIScreen.main.bounds.width
 let SCREENHEIGHT = UIScreen.main.bounds.height
-///git diff 的本质比较工作区的内容和本地版本库的不同
+///git diff HEAD 的本质比较工作区的内容和本地版本库的不同
 ///git checkout  --文件名 本质是放弃工作区的修改，前提是没有提交到缓存区
 ///git reset HEAD 文件名放弃缓存区的修改，然后直接git checkout  --文件名撤销修改
 
